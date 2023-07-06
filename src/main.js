61b141140d07109df7dac68e3217faead8cf90ad
CrazyBristy <132186467+CrazyBristy@users.noreply.github.com>
2023-07-06 15:40:00 +0800

    information

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("node:path");
+const { FORMAT } = require("sqlite3");
 
 const db = new sqlite3.Database(path.join(__dirname, "sample.db"), sqlite3.OPEN_READWRITE, (error) => {
     if (error){
@@ -33,6 +34,28 @@ function CallbackFunc(Error, Results){
     Results.forEach(data => console.log(data));
 }
 
-let sql = ``
-
-db.exec(sql, CallbackFunc);
+let sql = `
+CREATE TABLE IF NOT EXISTS Users(
+    ID TEXT PRIMARY KEY,
+    Username TEXT,
+    Emali TEXT,
+    DOB TEXT
+);SELECT name FROM pragma_table_info("Users");
+`
+let Ta = 'SELECT name FROM pragma_table_info("Users")'
+let c = `
+ALTER TABLE Users RENAME COLUMN Email TO Emali;`
+let d = `
+INSERT INTO Users (ID, Username, Email)
+VALUES ("13581","Bob","bob@csie.com")
+`
+//db.exec(d, CallbackFunc)
+//SELECT * FROM Users;
+let Table = `SELECT * FROM Users WHERE id = "13581"`;
+//db.all(Tabkes, CallbackFunc);
+//SELECT 13581 FROM Bob 
+//WHERE value = value;
+let Tables = `UPDATE Users SET Email = "okok" WHERE id ="13581"`;
+let Tabl = `DELETE FROM Users WHERE id = "13581"`;
+db.exec(Tabl, CallbackFunc);
+db.all(Table, CallbackFunc);
